e7b92246e0c5c5638b77a9cbf5e45fe26e9b3963
Supercip971 <supercyp971@gmail.com>
2021-01-08 09:41:55 -0400

    [type] added is_same

---
+++ src/type/is_same.h
@@ -0,0 +1,16 @@
+#ifndef IS_SAME_H
+#define IS_SAME_H
+#include "type/integral_constant.h"
+#include "type/remove.h"
+#include "ftypes.h"
+namespace fth {
+    template<class T1, class T2>
+    class is_same : public false_type {};
+
+    template<class T1>
+    class is_same<T1,T1> : public true_type {};
+
+    template <class T>
+    class is_nullptr : public is_same<nullptr_t,remove_const_volatile<T>>{};
+}
+#endif // IS_SAME_H
